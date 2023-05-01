1dee789451ea126d6007a1372292a73793504662
Benoit Gloria <gloria_b@epita.fr>
2023-05-01 17:12:10 +0200

    update go channel demo

--- go/channel.go
+++ go/channel.go
@@ -135,15 +135,61 @@ func buff4() {
 	wg.Wait()
 }
 
+func recv1(c <-chan int) {
+	fmt.Println("recv1: begin")
+	for i := range c {
+		fmt.Println("recv1:", i)
+	}
+	fmt.Println("recv1: end")
+}
+
+func recv2(c <-chan int) {
+	fmt.Println("recv2: begin")
+	for i := range c {
+		fmt.Println("recv2:", i)
+	}
+	fmt.Println("recv2: end")
+}
+
+func simple() {
+	c := make(chan int)
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go func() {
+		recv1(c)
+		wg.Done()
+	}()
+	go func() {
+		recv2(c)
+		wg.Done()
+	}()
+	time.Sleep(500 * time.Millisecond)
+	println("send", 0)
+	c <- 0
+	println("send", 1)
+	c <- 1
+	println("send", 2)
+	c <- 2
+	println("send", 3)
+	c <- 3
+	println("send close")
+	close(c)
+	println("send wait")
+	wg.Wait()
+	println("send end")
+}
+
 func main() {
-	fmt.Println("***** buff0 *****")
-	buff0()
-	fmt.Println("***** buff1 *****")
-	buff1()
-	fmt.Println("***** buff2 *****")
-	buff2()
-	fmt.Println("***** buff4 *****")
-	buff4()
-	fmt.Println("***** buff0TickTack *****")
-	buff0TickTack()
+	// fmt.Println("***** buff0 *****")
+	// buff0()
+	// fmt.Println("***** buff1 *****")
+	// buff1()
+	// fmt.Println("***** buff2 *****")
+	// buff2()
+	// fmt.Println("***** buff4 *****")
+	// buff4()
+	// fmt.Println("***** buff0TickTack *****")
+	// buff0TickTack()
+	fmt.Println("***** simple *****")
+	simple()
 }
